159a538974d3f7cfe1b72dd1a594aa2618645d1e
Interview Candidate <candidate@advanceview.io>
2026-04-21 19:55:03 +0300

    wip: 2026-04-21 16:55:03 UTC

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// BEN HARIKAYIM, CEVABI BULDUM
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
